67fd144fd4271081c686ed3bf75b218b34a2578e
Ivan Orekhov <i.orehov@mcplat.ru>
2021-02-02 13:30:46 +0300

    add body and params validations

--- src/app/controllers/wallets.controller.ts
+++ src/app/controllers/wallets.controller.ts
@@ -7,20 +7,30 @@ import {
   HttpResponseNotFound,
   HttpResponseOK,
   Post,
-  Put
+  Put,
+  ValidateBody,
+  ValidatePathParam
 } from '@foal/core';
 
 import { Wallet } from '../entities';
 
 export class WalletsController {
 
+  /**
+   * get all wallets
+   */
   @Get('/')
   async getAll() {
     const wallets = await Wallet.find();
     return new HttpResponseOK(wallets);
   }
 
+  /**
+   * get wallet by id
+   * @param ctx context
+   */
   @Get('/:id')
+  @ValidatePathParam('id', { type: 'number' })
   async getOne(ctx: Context) {
     const wallet = await Wallet.findOne({ id: ctx.request.params.id });
 
@@ -29,7 +39,20 @@ export class WalletsController {
       new HttpResponseOK(wallet);
   }
 
+  /**
+   * create wallet from model
+   * @param ctx context
+   */
   @Post('/')
+  @ValidateBody({
+    additionalProperties: false,
+    properties: {
+      name: { type: 'string' },
+      amount: { type: 'number' }
+    },
+    required: [ 'name' ],
+    type: 'object',
+  })
   async create(ctx: Context) {
     const wallet = new Wallet();
 
@@ -41,19 +64,42 @@ export class WalletsController {
     return new HttpResponseCreated(wallet);
   }
 
+  /**
+   * update wallet by id
+   * @param ctx context
+   */
   @Put('/:id')
+  @ValidatePathParam('id', { type: 'number' })
+  @ValidateBody({
+    additionalProperties: false,
+    properties: {
+      name: { type: 'string' },
+      amount: { type: 'number' }
+    },
+    required: [],
+    type: 'object',
+  })
   async update(ctx: Context) {
     const wallet = new Wallet();
 
-    wallet.name = ctx.request.body.name;
-    wallet.amount = ctx.request.body.amount;
+    if (ctx.request.body.name) {
+      wallet.name = ctx.request.body.name;
+    }
+    if (ctx.request.body.amount !== undefined) {
+      wallet.amount = ctx.request.body.amount;
+    }
 
     await Wallet.update(ctx.request.params.id, wallet);
 
     return new HttpResponseOK(wallet);
   }
 
+  /**
+   * remove wallet by id
+   * @param ctx context
+   */
   @Delete('/:id')
+  @ValidatePathParam('id', { type: 'number' })
   async remove(ctx: Context) {
     const wallet = await Wallet.findOne({ id: ctx.request.params.id });
 
