1cbfc64e3c0dccdb5ecd2b4b735db97ff7151f5e
Ivan Orekhov <makito82@gmail.com>
2021-02-03 20:45:49 +0300

    update migrations

--- src/migrations/1612348831914-migration.ts
+++ src/migrations/1612348831914-migration.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class migration1612263855467 implements MigrationInterface {
-    name = 'migration1612263855467'
+export class migration1612348831914 implements MigrationInterface {
+    name = 'migration1612348831914'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `wallets` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(25) NOT NULL DEFAULT '', `amount` float(10,2) NOT NULL DEFAULT '0.00', PRIMARY KEY (`id`)) ENGINE=InnoDB");
